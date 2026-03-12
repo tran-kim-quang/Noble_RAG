997b39777817096e9f72d89be79012f48f57a6da
tran-kim-quang <trankimquang1603@gmail.com>
2026-03-13 04:19:29 +0700

    feat: build RAG service

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import sys
+# import sys
 
-from client.whisper_client import WhisperClient
+# from client.whisper_client import WhisperClient
 
 
-client = WhisperClient(url="http://localhost:8001")
+# client = WhisperClient(url="http://localhost:8001")
 
-ref_wav_path = "test/ref.wav"
+# ref_wav_path = "test/ref.wav"
 
-# 3. Gọi hàm nhận diện
-text = client.transcribe(str(ref_wav_path))
+# # 3. Gọi hàm nhận diện
+# text = client.transcribe(str(ref_wav_path))
 
-if text:
-    print(f"Văn bản: {text}")
+# if text:
+#     print(f"Văn bản: {text}")
+
+
+from client.rag_client import RAGClient
+
+rag_client = RAGClient(url="http://localhost:8000")
+rag_client.upload_document()
